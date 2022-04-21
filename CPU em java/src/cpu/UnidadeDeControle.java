26210a446cb70ff96dc1eb7d8d8e80d77dea55a6
unknown <leo091945@gmail.com>
2022-04-21 19:56:47 -0300

    Pipeline

--- CPU em java/src/cpu/UnidadeDeControle.java
+++ CPU em java/src/cpu/UnidadeDeControle.java
@@ -4,37 +4,68 @@ import java.util.List;
 
 public class UnidadeDeControle {
 	
-	private int pc = 0;
+	private int pc = 0; // Registradores da UC
 	private String ir;
 	private String mar;
+	
 	private ULA ula;
+	
 	
 	public void rollMemory(List<String> mem) {
 		ula = new ULA();
-		for(pc = 0; pc < mem.size();pc++) {
-			mar = mem.get(pc);//Armazena em mar a instrucao e a referencia de operandos
-			if(mar.length()  > 6) {
-				ir = mar.substring(0,4);//Armazena instrucao atual
-				ula.mbr = ir;//Passa para a Ula a instrucao
-				
-				if(mar.substring(4, 6).equals( "0f")) {//Passando endereco de memoria, Unidade de controle le memoria
-					int pos = Integer.parseInt(mar.substring(6,10));
-					ula.mq = mem.get(pos);//Passa os operandos para a ula
-				}else {
-					ula.mq = mar.substring(4,10);
+		nextPc(-1,mem);
+	}
+	
+	public void nextPc(int i,List<String> mem) {//Fetch next operation
+		pc = i;
+		nextIr(i+1,mem);
+	}
+	
+	public void nextIr(final int i,List<String> mem) {//Fetch next word
+		if(i < mem.size()){
+			ir = mem.get(i);
+			nextMar(ir,mem, i);
+			new Thread() {
+				public final int pos = i;
+				public void run() {
+					nextPc(pos, mem);
 				}
-				
-				if(mar.substring(10, 12).equals( "0f")) {//Passando endereco de memoria, Unidade de controle le memoria
-					int pos = Integer.parseInt(mar.substring(12,16));
-					ula.ac = mem.get(pos);//Passa os operandos para a ula
-				}else {
-					ula.ac = mar.substring(10,16);//Passa os operandos para a ula
-				}
-				
-				ula.calculate();//Ula decodifica e opera dados.
-				mem.add(ula.ac);//Salva novo dado na memoria
-			}
+			}.start();;
 		}
 	}
 	
+	public void nextMar(String ir,List<String> mem,int i) {//Fetch next operands
+		mar = ir;
+		ula.mbr = ir.substring(0,4);
+		if(mar.length() > 6) {
+			if(mar.substring(4, 6).equals( "0f")) {//Passando endereco de memoria, Unidade de controle le memoria
+				int pos = Integer.parseInt(mar.substring(6,10));
+				ula.mq = mem.get(pos);//Passa os operandos para a ula
+			}else {
+				ula.mq = mar.substring(4,10);
+			}
+			
+			if(mar.substring(10, 12).equals( "0f")) {//Passando endereco de memoria, Unidade de controle le memoria
+				int pos = Integer.parseInt(mar.substring(12,16));
+				ula.ac = mem.get(pos);//Passa os operandos para a ula
+			}else {
+				ula.ac = mar.substring(10,16);//Passa os operandos para a ula
+			}
+			nextCalc(mem,i);
+		}
+	
+	}
+	
+	public void nextCalc(List<String> mem,int i) {//Fetch next calc
+		System.out.println("-----------------------------------------------------------------------------------------");
+		ula.calculate();
+		nextWrite(mem,i);
+	}
+	
+	public void nextWrite(List<String> mem, int i) {//Fetch next write
+		System.out.println("Salvando dados " + (pc+1));
+		System.out.println("Registradores estão salvos da seguinte forma PC: " + (i+1) + " IR : " + mem.get(i+1) +" MAR " + mem.get(i+1));
+		System.out.println("-----------------------------------------------------------------------------------------");
+		mem.add(ula.ac);
+	}
 }
--- CPU em java/src/cpu/main.java
+++ CPU em java/src/cpu/main.java
@@ -8,39 +8,43 @@ public class main {
 		
 		mem = new Memoria();
 		//Inserindo dados na memoria
+		/*
 		mem.memoria.add("000001");//0
 		mem.memoria.add("000010");//1
 		mem.memoria.add("000100");//2
-		
+		*/
 		//Somando/subtraindo dados na memoria
 		//Não é necessário ordem na soma e subtracao
 		
+		/*
 		mem.memoria.add("00000f00000f0001");//3  //2 + 1
 		mem.memoria.add("0000000101100010");//4  //5 - 2
 		mem.memoria.add("00000f0001000001");//5  //2 + 1
 		mem.memoria.add("00000f0002000001");//6  //4 + 1
-
+		*/
+		
 		//Multiplicando dados na memoria
 		//Não é necessário ordem na multiplicacao
 		
+		/*
 		mem.memoria.add("1000000010000101");//7 5x2
 		mem.memoria.add("1000000010000010");//8 2x2
 		mem.memoria.add("1000000010000011");//9 2x3
 		mem.memoria.add("1000000010000100");//10 2x4
 		mem.memoria.add("1000000100000001");//11 4x1
 		mem.memoria.add("10000f00000f0001");//12 Multiplicando dois numeros já salvos na memoria 
-		mem.memoria.add("1000100010000010");	//13 Multiplicando -2x2
-		
+		mem.memoria.add("1000100010000010");//13 Multiplicando -2x2
+		*/
 		//Trabalhar com numeros na memoria nessa forma pode se tornar muito complexo por não saber onde eles estão alocados
 
 		//Divisão
 		//Ordem divisor, dividendo
 		
-		mem.memoria.add("1001000101000111");	//14	7/5
-		mem.memoria.add("1001000010000101");	//15	5/2
-		mem.memoria.add("1001000001000010");	//16	2/1
-		mem.memoria.add("1001000100000111");	//17	7/4
-		mem.memoria.add("1001100010000100");	//18	4/-2
+		mem.memoria.add("1001000101000111");	//14	7 / 5
+		mem.memoria.add("1000000100000010");	//15	4 * 2
+		mem.memoria.add("0000000001000010");	//16	1 + 2
+		mem.memoria.add("0000100100000111");	//17   -2 + 7
+		mem.memoria.add("1001100010000100");	//18	4 /-2
 		
 		
 		ULA ula = new ULA();
